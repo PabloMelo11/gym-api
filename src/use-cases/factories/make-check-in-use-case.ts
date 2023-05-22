f72d1562642ff9978df9c8b5cc5bcfcd76a95945
PabloMelo <pablomelodev@gmail.com>
2023-05-22 10:49:17 -0300

    feat: create factories use case

---
+++ src/use-cases/factories/make-check-in-use-case.ts
@@ -0,0 +1,13 @@
+import { PrismaCheckInsRepository } from '@/repositories/prisma/prisma-check-ins-repository';
+import { PrismaGymsRepository } from '@/repositories/prisma/prisma-gyms-repository';
+import { CheckInUseCase } from '../check-in';
+
+export function makeGetUserMetricsUseCase() {
+  const prismaCheckInsRepository = new PrismaCheckInsRepository();
+  const prismaGymsRepository = new PrismaGymsRepository();
+  const useCase = new CheckInUseCase(
+    prismaCheckInsRepository,
+    prismaGymsRepository
+  );
+  return useCase;
+}
