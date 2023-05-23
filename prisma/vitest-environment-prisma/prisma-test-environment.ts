02c13fb7dcb578d12a1626e606215b6be3168ef8
PabloMelo <pablomelodev@gmail.com>
2023-05-23 17:13:44 -0300

    feat: create database to test

--- prisma/vitest-environment-prisma/prisma-test-environment.ts
+++ prisma/vitest-environment-prisma/prisma-test-environment.ts
@@ -1,4 +1,12 @@
+import 'dotenv/config';
+
+import { randomUUID } from 'node:crypto';
+import { URL } from 'node:url';
+import { execSync } from 'node:child_process';
 import { Environment } from 'vitest';
+import { PrismaClient } from '@prisma/client';
+
+const prisma = new PrismaClient();
 
 /**
  * #1 - execute npm link in this path
@@ -6,13 +14,34 @@ import { Environment } from 'vitest';
  * #3 - execute npm link vitest-environment-prisma
  */
 
+function generateDatabaseUrl(schema: string) {
+  if (!process.env.DATABASE_URL) {
+    throw new Error('Please provide a DATABASE_URL environment variable.');
+  }
+
+  const url = new URL(process.env.DATABASE_URL);
+
+  url.searchParams.set('schema', schema);
+
+  return url.toString();
+}
+
 export default <Environment>{
   name: 'prisma',
   async setup() {
-    console.log('setup');
+    const schema = randomUUID();
+    const databaseURL = generateDatabaseUrl(schema);
+
+    process.env.DATABASE_URL = databaseURL;
+
+    execSync('npx prisma migrate deploy');
+
     return {
-      teardown() {
-        console.log('teardown');
+      async teardown() {
+        await prisma.$executeRawUnsafe(
+          `DROP SCHEMA IF EXISTS "${schema}" CASCADE`
+        );
+        await prisma.$disconnect();
       },
     };
   },
