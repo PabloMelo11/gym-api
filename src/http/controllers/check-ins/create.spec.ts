2eb4fb0a242a679fb1d5fe81a77f8e83ac7cd9f0
PabloMelo <pablomelodev@gmail.com>
2023-05-26 16:50:31 -0300

    feat: test controllers check-ins

--- src/http/controllers/check-ins/create.spec.ts
+++ src/http/controllers/check-ins/create.spec.ts
@@ -2,8 +2,9 @@ import request from 'supertest';
 import { describe, it, expect, beforeAll, afterAll } from 'vitest';
 import { app } from '@/app';
 import { createAndAuthenticateUser } from '@/utils/test/create-and-authenticate-user';
+import { prisma } from '@/lib/prisma';
 
-describe('Create Gym (e2e)', () => {
+describe('Create Check-in (e2e)', () => {
   beforeAll(async () => {
     await app.ready();
   });
@@ -12,16 +13,21 @@ describe('Create Gym (e2e)', () => {
     await app.close();
   });
 
-  it('should be able to create new gym', async () => {
+  it('should be able to create new check-in', async () => {
     const { token } = await createAndAuthenticateUser(app);
 
+    const gym = await prisma.gym.create({
+      data: {
+        title: 'JavaScript Gym',
+        latitude: -22.4583086,
+        longitude: -47.0723589,
+      },
+    });
+
     const response = await request(app.server)
-      .post('/gyms')
+      .post(`/gyms/${gym.id}/check-ins`)
       .set('Authorization', `Bearer ${token}`)
       .send({
-        title: 'JavaScript Gym',
-        description: 'Some description',
-        phone: '199999999',
         latitude: -22.4583086,
         longitude: -47.0723589,
       });
