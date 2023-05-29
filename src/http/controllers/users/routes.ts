201a3d9cbc199bdb9e8c7357e63b35d8641ad707
PabloMelo <pablomelodev@gmail.com>
2023-05-29 16:54:27 -0300

    feat: create route refresh token

--- src/http/controllers/users/routes.ts
+++ src/http/controllers/users/routes.ts
@@ -3,10 +3,13 @@ import { verifyJWT } from '@/http/middlewares/verify-jwt';
 import { register } from './register';
 import { authenticate } from './authenticate';
 import { profile } from './profile';
+import { refreshToken } from './refresh-token';
 
 export async function usersRoutes(app: FastifyInstance) {
   app.post('/users', register);
   app.post('/sessions', authenticate);
+
+  app.patch('/token/refresh', refreshToken);
 
   app.get('/me', { onRequest: [verifyJWT] }, profile);
 }
